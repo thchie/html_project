fd4be35219e20a7094eaebc95526e9455b0db571
thchie <hueychie@gmail.com>
2024-01-23 14:41:26 +0800

    Add files via upload

---
+++ T5/web gred.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Senarai Gred Markah</title>
+</head>
+<body>
+	<p>Senarai Markah dan Gred</p>
+
+	<?php
+	
+	//membuka fail untuk membaca kandungan fail
+	$f = fopen("Matematik.txt","r");
+	$valid = false; 		// menilai awalkan dengan nilai false
+
+
+
+	?>
+</body>
+</html>
